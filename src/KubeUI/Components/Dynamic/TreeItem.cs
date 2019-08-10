e8ca1890c33f63bb09b69f301ab09506044b4e13
Ivan Josipovic <ivanjosipovic@outlook.com>
2019-08-09 21:58:41 -0700

    Testing new Tree Options

---
+++ src/KubeUI/Components/Dynamic/TreeItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KubeUI.Components.Dynamic
+{
+    public class TreeItem
+    {
+        public AttributeCollection Attributes { get; set; }
+
+        public string Name { get; set; }
+
+        public object Parent { get; set; }
+
+        public object Object { get; set; }
+
+        public string Summary { get; set; }
+
+        public bool IsCollection { get; set; }
+
+        public bool IsCollectionItem { get; set; }
+
+        public bool HideLink { get; set; }
+
+        public bool Minimized { get; set; }
+
+        public List<TreeItem> Children { get; set; } = new List<TreeItem>();
+    }
+
+}
