8f37510ce9fae8aeac6f22be15e737fdc5f17f15
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2019-10-15 19:13:35 -0700

    wip

---
+++ src/KubeUI2/Utillities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KubeUI2
+{
+    public static class Utillities
+    {
+        public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> source)
+        {
+            return source ?? Enumerable.Empty<T>();
+        }
+
+        public static IDictionary<T, V> OrEmptyIfNull<T, V>(this IDictionary<T, V> source)
+        {
+            return source ?? new Dictionary<T, V>();
+        }
+    }
+}
