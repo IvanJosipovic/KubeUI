9a62efcdd3e240508b272e7a2d0d2f4abbcd081f
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2019-11-16 15:32:23 -0800

    wip

--- src/KubeUI2/Components/Types/Deployment/DeploymentList.razor.cs
+++ src/KubeUI2/Components/Types/Deployment/DeploymentList.razor.cs
@@ -2,6 +2,8 @@
 using k8s.Models;
 using KubeUI.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,6 +15,9 @@ namespace KubeUI2.Components.Types
     {
         [Parameter]
         public string Namespace { get; set; }
+
+        [Inject]
+        protected ILogger<DeploymentList> Logger { get; set; }
 
         [Inject]
         protected IState State { get; set; }
@@ -37,6 +42,50 @@ namespace KubeUI2.Components.Types
             {
                 Items = (await Client.ListNamespacedDeploymentAsync(State.Namespace))?.Items;
             }
+        }
+
+        public async Task Delete(V1Deployment item)
+        {
+            await Client.DeleteNamespacedDeploymentAsync(item.Metadata.Name, item.Metadata.NamespaceProperty);
+
+            await Update();
+        }
+
+        private async Task ScaleUp(V1Deployment item)
+        {
+            int replicas = 0;
+
+            if (item.Spec.Replicas.HasValue)
+	        {
+                replicas = item.Spec.Replicas.Value;
+            }
+
+            replicas++;
+
+            var patch = new JsonPatchDocument<V1Deployment>();
+            patch.Replace(e => e.Spec.Replicas, replicas);
+
+            await Client.PatchNamespacedDeploymentScaleAsync(new V1Patch(patch), item.Metadata.Name, item.Metadata.NamespaceProperty);
+
+            await Update();
+        }
+
+        private async Task ScaleDown(V1Deployment item)
+        {
+            if (!item.Spec.Replicas.HasValue)
+            {
+                return;
+            }
+
+            int replicas = item.Spec.Replicas.Value;
+
+            replicas--;
+
+            var patch = new JsonPatchDocument<V1Deployment>();
+            patch.Replace(e => e.Spec.Replicas, replicas);
+
+            await Client.PatchNamespacedDeploymentScaleAsync(new V1Patch(patch), item.Metadata.Name, item.Metadata.NamespaceProperty);
+            await Update();
         }
     }
 }
