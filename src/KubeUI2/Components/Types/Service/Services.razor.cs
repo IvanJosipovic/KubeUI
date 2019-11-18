584ebe72130c26452f9c9b0150fd3efdacd57c6d
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2019-11-18 11:41:37 -0800

    wip

---
+++ src/KubeUI2/Components/Types/Service/Services.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace KubeUI2.Components.Types
+{
+    [Route("/Services")]
+    [Route("/{Namespace}/Services")]
+    public partial class Services
+    {
+        [Parameter]
+        public string Namespace { get; set; }
+    }
+}
