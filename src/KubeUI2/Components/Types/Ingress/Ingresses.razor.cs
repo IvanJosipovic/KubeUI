0fb03f4a851315c77bd80d69dd0686b9625b19e4
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2019-11-18 17:42:09 -0800

    Added Ingress

---
+++ src/KubeUI2/Components/Types/Ingress/Ingresses.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace KubeUI2.Components.Types
+{
+    [Route("/Ingresses")]
+    [Route("/{Namespace}/Ingresses")]
+    public partial class Ingresses
+    {
+        [Parameter]
+        public string Namespace { get; set; }
+    }
+}
