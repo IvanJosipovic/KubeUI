d8c0d132811d5a9ddea01f39554d76e56e7b7892
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2019-12-16 18:13:57 -0800

    Added Secrets

---
+++ src/KubeUI.Core/Components/Types/Secrets/Secrets.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace KubeUI.Core.Components.Types
+{
+    [Route("/Secrets")]
+    [Route("/{Namespace}/Secrets")]
+    public partial class Secrets
+    {
+        [Parameter]
+        public string Namespace { get; set; }
+    }
+}
