75a8f3367ecf8c219bb1db1aaf11b8b816e63adf
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2020-01-07 18:28:18 -0800

    Added DaemonSet

---
+++ src/KubeUI.Core/Components/Types/DaemonSet/DaemonSet.razor.cs
@@ -0,0 +1,35 @@
+﻿using k8s;
+using k8s.Models;
+using KubeUI.Services;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace KubeUI.Core.Components.Types
+{
+    [Route("/{Namespace}/DaemonSet/{Name}")]
+    public partial class DaemonSet
+    {
+        [Parameter]
+        public string Namespace { get; set; }
+
+        [Parameter]
+        public string Name { get; set; }
+
+        [Inject]
+        protected IKubernetes Client { get; set; }
+
+        private V1DaemonSet Item;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await Update();
+        }
+
+        private async Task Update()
+        {
+            Item = await Client.ReadNamespacedDaemonSetAsync(Name, Namespace);
+
+            StateHasChanged();
+        }
+    }
+}
