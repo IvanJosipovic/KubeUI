cecf0743f820e7fdb1ace79fbf8ab7c927755ec9
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2022-08-11 17:49:30 -0700

    feat: display XKubernetesPreserveUnknownFields as Yaml

--- src/KubeUI.Core/Components/KubeMonacoEditor.razor.cs
+++ src/KubeUI.Core/Components/KubeMonacoEditor.razor.cs
@@ -3,7 +3,7 @@ using BlazorMonaco;
 
 namespace KubeUI.Core.Components
 {
-    public partial class MonacoViewer : IDisposable
+    public partial class KubeMonacoEditor : IDisposable
     {
         [Parameter]
         public string Language { get; set; } = "yaml";
@@ -11,11 +11,21 @@ namespace KubeUI.Core.Components
         [Parameter]
         public string Value { get; set; }
 
+        [Parameter]
+        public bool ReadOnly { get; set; }
+
         private MonacoEditor _editor { get; set; }
 
         private StandaloneEditorConstructionOptions EditorConstructionOptions(MonacoEditor editor)
         {
-            return new StandaloneEditorConstructionOptions { Language = Language, GlyphMargin = true, Value = Value, AutomaticLayout = true };
+            return new StandaloneEditorConstructionOptions
+            {
+                Language = Language,
+                GlyphMargin = true,
+                Value = Value,
+                AutomaticLayout = true,
+                ReadOnly = ReadOnly
+            };
         }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
