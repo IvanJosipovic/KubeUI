22858b73057a2a792933c2e257013e4e6c54a081
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2022-08-29 19:24:07 -0700

    feat: compare ignores noisy metadata and status

--- src/KubeUI.Core.Tests/ObjectCompare.cs
+++ src/KubeUI.Core.Tests/ObjectCompare.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using k8s.Models;
 using KubeUI.Core.Client;
 
@@ -78,6 +79,57 @@ namespace KubeUI.Core.Tests
             var result = ObjectCompare.CompareObjects(left, right);
 
             var kind = result.Kind;
+        }
+
+        [Fact]
+        public void CleanMetadata()
+        {
+            var left = new V1Deployment()
+            {
+                Metadata = new V1ObjectMeta
+                {
+                    Name = "test",
+                    Generation = 213,
+                    CreationTimestamp = DateTime.UtcNow,
+                    Finalizers = new List<string>() { "test" },
+                    ManagedFields = new List<V1ManagedFieldsEntry>() { new V1ManagedFieldsEntry() { Operation = "test" } },
+                    ResourceVersion = "1",
+                    SelfLink = "asd",
+                    Uid = "asd"
+                },
+            };
+
+            var result = ObjectCompare.CleanObject(left);
+
+            result.Metadata.Generation.Should().BeNull();
+            result.Metadata.CreationTimestamp.Should().BeNull();
+            result.Metadata.Finalizers.Should().BeNull();
+            result.Metadata.ManagedFields.Should().BeNull();
+            result.Metadata.ResourceVersion.Should().BeNull();
+            result.Metadata.SelfLink.Should().BeNull();
+            result.Metadata.Uid.Should().BeNull();
+        }
+
+        [Fact]
+        public void CleanStatus()
+        {
+            var left = new V1Deployment()
+            {
+                Metadata = new V1ObjectMeta
+                {
+                    Name = "test"
+                },
+                Status = new V1DeploymentStatus()
+                {
+                    AvailableReplicas = 1,
+                }
+            };
+
+            var result = ObjectCompare.CleanObject(left);
+
+            var prop = result.GetType().GetProperty("Status");
+
+            prop.GetValue(result).Should().BeNull();
         }
     }
 }
--- src/KubeUI.Core/Client/ObjectCompare.cs
+++ src/KubeUI.Core/Client/ObjectCompare.cs
@@ -7,13 +7,36 @@ namespace KubeUI.Core.Client
     {
         public static JsonDiffDelta CompareObjects(IKubernetesObject<V1ObjectMeta> left, IKubernetesObject<V1ObjectMeta> right)
         {
-            var diff = JsonDiffPatcher.Diff(KubernetesJson.Serialize(left), KubernetesJson.Serialize(right), new JsonDiffOptions
+            var leftJson = KubernetesJson.Serialize(CleanObject(left)); ;
+            var rightJson = KubernetesJson.Serialize(CleanObject(right));
+
+            var diff = JsonDiffPatcher.Diff(leftJson, rightJson, new JsonDiffOptions
             {
                 JsonElementComparison = JsonElementComparison.Semantic,
                 SuppressDetectArrayMove = true,
                 ArrayObjectItemMatchByPosition = true
             });
             return new JsonDiffDelta(diff);
+        }
+
+        public static IKubernetesObject<V1ObjectMeta> CleanObject(IKubernetesObject<V1ObjectMeta> kubeObject)
+        {
+            kubeObject.Metadata.Generation = null;
+            kubeObject.Metadata.CreationTimestamp = null;
+            kubeObject.Metadata.Finalizers = null;
+            kubeObject.Metadata.ManagedFields = null;
+            kubeObject.Metadata.ResourceVersion = null;
+            kubeObject.Metadata.SelfLink = null;
+            kubeObject.Metadata.Uid = null;
+
+            var prop = kubeObject.GetType().GetProperty(nameof(V1Deployment.Status));
+
+            if (prop != null)
+            {
+                prop.SetValue(kubeObject, null);
+            }
+
+            return kubeObject;
         }
     }
 }
