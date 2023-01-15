ecbbd88129716c97589c5669de90cc2519924915
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-01-15 10:49:46 -0800

    chore: add code to download Kind

---
+++ src/KubeUI.Core.Tests/Kind.cs
@@ -0,0 +1,154 @@
+﻿using k8s;
+using k8s.KubeConfigModels;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace KubeUI.Core.Tests;
+
+public class Kind
+{
+    public string Version = "0.17.0";
+
+    public async Task DownloadClient()
+    {
+        var client = new HttpClient();
+        var url = string.Empty;
+        var path = "kind" + (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ".exe" : "");
+
+        if (System.IO.File.Exists(path)) return;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            // Mac
+            url = $"https://kind.sigs.k8s.io/dl/v{Version}/kind-darwin-arm64";
+
+            if (RuntimeInformation.ProcessArchitecture == Architecture.X64)
+            {
+                url = $"https://kind.sigs.k8s.io/dl/v{Version}/kind-darwin-amd64";
+            }
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            // Linux
+            url = $"https://kind.sigs.k8s.io/dl/v{Version}/kind-linux-amd64";
+        }
+        else
+        {
+            // Windows
+            url = $"https://kind.sigs.k8s.io/dl/v{Version}/kind-windows-amd64";
+        }
+
+        var bytes = await client.GetByteArrayAsync(url);
+
+        System.IO.File.WriteAllBytes(path, bytes);
+
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = "chmod",
+                Arguments = "+x ./kind",
+                CreateNoWindow = true
+            };
+
+            var proc = new Process() { StartInfo = startInfo };
+            proc.Start();
+        }
+    }
+
+    public void CreateCluster(string name, string? image = null)
+    {
+        var p = new Process();
+        p.StartInfo.FileName = "kind.exe";
+        p.StartInfo.Arguments = $"create cluster --name {name}" + (string.IsNullOrEmpty(image) ? "" : $" --image {image}");
+        p.StartInfo.RedirectStandardError = true;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = true;
+        p.Start();
+        var error = p.StandardError.ReadToEnd();
+
+        if (!string.IsNullOrEmpty(error) && error.StartsWith("ERROR:"))
+        {
+            throw new Exception(error);
+        }
+    }
+
+    public void DeleteCluster(string name)
+    {
+        var p = new Process();
+        p.StartInfo.FileName = "kind.exe";
+        p.StartInfo.Arguments = $"delete cluster --name {name}";
+        p.StartInfo.RedirectStandardError = true;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = true;
+        p.Start();
+        var error = p.StandardError.ReadToEnd();
+
+        if (!string.IsNullOrEmpty(error) && error.StartsWith("ERROR:"))
+        {
+            throw new Exception(error);
+        }
+    }
+
+    public List<string> GetClusters()
+    {
+        var p = new Process();
+        p.StartInfo.FileName = "kind.exe";
+        p.StartInfo.Arguments = $"get clusters";
+        p.StartInfo.RedirectStandardOutput = true;
+        p.StartInfo.RedirectStandardError = true;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = true;
+        p.Start();
+        var result = p.StandardOutput.ReadToEnd();
+        var error = p.StandardError.ReadToEnd();
+
+        if (!string.IsNullOrEmpty(error))
+        {
+            throw new Exception(error);
+        }
+
+        return new List<string>(result.TrimEnd().Split("\n"));
+    }
+
+    public string GetKubeConfig(string name)
+    {
+        var p = new Process();
+        p.StartInfo.FileName = "kind.exe";
+        p.StartInfo.Arguments = $"get kubeconfig --name {name}";
+        p.StartInfo.RedirectStandardOutput = true;
+        p.StartInfo.RedirectStandardError = true;
+        p.StartInfo.UseShellExecute = false;
+        p.StartInfo.CreateNoWindow = true;
+        p.Start();
+        var result = p.StandardOutput.ReadToEnd();
+        var error = p.StandardError.ReadToEnd();
+
+        if (!string.IsNullOrEmpty(error))
+        {
+            throw new Exception(error);
+        }
+
+        return result;
+    }
+
+    public K8SConfiguration GetK8SConfiguration(string name)
+    {
+        return KubernetesYaml.Deserialize<K8SConfiguration>(GetKubeConfig(name));
+    }
+
+    public Kubernetes GetKubernetesClient(string name)
+    {
+        return new Kubernetes(KubernetesClientConfiguration.BuildConfigFromConfigObject(GetK8SConfiguration(name)));
+    }
+
+    public void DeleteAllClusters()
+    {
+        foreach (var cluster in GetClusters())
+        {
+            DeleteCluster(cluster);
+        }
+    }
+}
---
+++ src/KubeUI.Core.Tests/TestHarness.cs
@@ -0,0 +1,44 @@
+﻿using k8s;
+
+namespace KubeUI.Core.Tests;
+
+public class TestHarness : IDisposable
+{
+    public IServiceProvider ServiceProvider { get; set; }
+
+    public string Name { get; set; } = Guid.NewGuid().ToString();
+
+    public ICluster Cluster { get; set; }
+
+    public Kubernetes Kubernetes { get; set; }
+
+    private Kind Kind { get; set; }
+
+    public string Version { get; set; } = "kindest/node:v1.25.3";
+
+    public TestHarness()
+    {
+        Kind = new Kind();
+        Kind.DownloadClient().Wait();
+        Kind.CreateCluster(Name, Version);
+        var kc = Kind.GetKubeConfig(Name);
+
+        IServiceCollection sc = new ServiceCollection();
+        ConfigureServices.Configure(null, sc);
+
+        ServiceProvider = sc.BuildServiceProvider();
+
+        var cm = ServiceProvider.GetRequiredService<ClusterManager>();
+
+        cm.LoadFromConfig(kc);
+
+        Cluster = cm.GetCluster($"kind-{Name}");
+
+        Kubernetes = Kind.GetKubernetesClient(Name);
+    }
+
+    public void Dispose()
+    {
+        Kind.DeleteCluster(Name);
+    }
+}
