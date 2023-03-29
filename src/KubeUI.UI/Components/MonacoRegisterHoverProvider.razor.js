05d7512dc61c54d0c15eec4cca46f91b176a64ac
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-03-29 13:43:39 -0700

    fix: remove uneeded import

--- src/KubeUI.UI/Components/MonacoRegisterHoverProvider.razor.js
+++ src/KubeUI.UI/Components/MonacoRegisterHoverProvider.razor.js
@@ -1,5 +1,3 @@
-import { getDocumentSymbols } from 'monaco-editor/esm/vs/editor/contrib/documentSymbols/documentSymbols';
-
 export function registerHoverProvider(languageId, hoverProvider) {
     monaco.languages.registerHoverProvider(languageId, {
         provideHover: function (model, position) {
