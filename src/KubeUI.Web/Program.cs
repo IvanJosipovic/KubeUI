3b85ce8cd767c0e802dbdb9d97ff6bc2b7c23de7
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-05-16 21:23:20 -0700

    chore: remove top level

--- src/KubeUI.Web/Program.cs
+++ src/KubeUI.Web/Program.cs
@@ -2,14 +2,23 @@ using KubeUI.UI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace KubeUI.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-ConfigureServices.Configure(builder.Configuration, builder.Services);
-KubeUI.Core.Client.ConfigureServices.Configure(builder.Configuration, builder.Services);
-builder.Services.AddHttpClient(string.Empty, client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+            UI.ConfigureServices.Configure(builder.Configuration, builder.Services);
+            KubeUI.Core.Client.ConfigureServices.Configure(builder.Configuration, builder.Services);
+            builder.Services.AddHttpClient(string.Empty, client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
-builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
+    }
+}
