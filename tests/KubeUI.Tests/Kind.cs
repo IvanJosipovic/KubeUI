62c5fcd611eaf1dda7fd6074fd7617d245c75ce8
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-06-02 17:19:34 -0700

    chore: update KinD

--- tests/KubeUI.Tests/Kind.cs
+++ tests/KubeUI.Tests/Kind.cs
@@ -8,7 +8,7 @@ namespace KubeUI.Core.Tests;
 
 public class Kind
 {
-    public string Version = "0.22.0";
+    public string Version = "0.23.0";
 
     public string FileName { get; } = "kind" + (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ".exe" : "");
 
