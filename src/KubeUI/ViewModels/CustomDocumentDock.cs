e3e5348a2bf560d989ffcecb1460ef162ec2daee
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-06-23 21:21:27 -0700

    chore: disable new document in dock

--- src/KubeUI/ViewModels/CustomDocumentDock.cs
+++ src/KubeUI/ViewModels/CustomDocumentDock.cs
@@ -1,6 +1,4 @@
-﻿using Avalonia;
-using CommunityToolkit.Mvvm.Input;
-using Dock.Model.Mvvm.Controls;
+﻿using Dock.Model.Mvvm.Controls;
 
 namespace KubeUI.ViewModels;
 
@@ -8,25 +6,6 @@ public sealed class CustomDocumentDock : DocumentDock
 {
     public CustomDocumentDock()
     {
-        CreateDocument = new RelayCommand(CreateNewDocument);
-    }
-
-    private void CreateNewDocument()
-    {
-        if (!CanCreateDocument)
-        {
-            return;
-        }
-
-        var index = VisibleDockables?.Count + 1;
-        var document = Application.Current.GetRequiredService<ClusterListViewModel>();
-        document.Title = $"Document{index}";
-        document.Id = $"Document{index}";
-
-        //new DocumentViewModel { Id = $"Document{index}", Title = $"Document{index}" };
-
-        Factory?.AddDockable(this, document);
-        Factory?.SetActiveDockable(document);
-        Factory?.SetFocusedDockable(this, document);
+        CanCreateDocument = false;
     }
 }
