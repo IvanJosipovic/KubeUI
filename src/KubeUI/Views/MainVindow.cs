7be4c5278608a0bb085b9c8d2c3bae2ba3e7a33c
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-09-08 12:18:42 -0700

    chore: cleanup

--- src/KubeUI/Views/MainVindow.cs
+++ src/KubeUI/Views/MainVindow.cs
@@ -9,51 +9,46 @@ public static class MainWindow
             .Title("KubeUI")
             .UseLayoutRounding(true)
             .SetProp(NativeMenu.MenuProperty, new NativeMenu()
-                                                    .Items([
-                                                        new NativeMenuItem()
-                                                            .Header(Resources.MainView_Menu_File)
-                                                            .Items([
-                                                                new NativeMenuItem()
-                                                                    .Header(Resources.MainView_Menu_File_Clusters)
-                                                                    .Command(vm.OpenClustersCommand),
-                                                                new NativeMenuItem()
-                                                                    .Header(Resources.MainView_Menu_File_Settings)
-                                                                    .Command(vm.OpenSettingsCommand),
+                .Items([
+                    new NativeMenuItem()
+                        .Header(Resources.MainView_Menu_File)
+                        .Items([
+                            new NativeMenuItem()
+                                .Header(Resources.MainView_Menu_File_Clusters)
+                                .Command(vm.OpenClustersCommand),
+                            new NativeMenuItem()
+                                .Header(Resources.MainView_Menu_File_Settings)
+                                .Command(vm.OpenSettingsCommand),
 
-                                                                new NativeMenuItemSeparator(),
+                            new NativeMenuItemSeparator(),
 
-                                                                new NativeMenuItem()
-                                                                    .Header(Resources.MainView_Menu_File_Exit)
-                                                                    .Command(vm.CloseCommand),
-                                                                ]),
-                                                        new NativeMenuItem()
-                                                            .Header(Resources.MainView_Menu_Window)
-                                                            .Items([
-                                                                new NativeMenuItem()
-                                                                    .Header(Resources.MainView_Menu_Window_ResetLayout)
-                                                                    .Command(vm.ResetLayoutCommand),
-                                                                new NativeMenuItem()
-                                                                    .Header(Resources.MainView_Menu_Window_SwitchTheme)
-                                                                    .Command(vm.SwitchThemeCommand),
-                                                                ]),
-                                                        new NativeMenuItem()
-                                                            .Header(Resources.MainView_Menu_Help)
-                                                            .Items([
-                                                                new NativeMenuItem()
-                                                                    .Header(Resources.MainView_Menu_Help_About)
-                                                                    .Command(vm.OpenAboutCommand),
-                                                                ])
-                                                        ])
+                            new NativeMenuItem()
+                                .Header(Resources.MainView_Menu_File_Exit)
+                                .Command(vm.CloseCommand),
+                            ]),
+                    new NativeMenuItem()
+                        .Header(Resources.MainView_Menu_Window)
+                        .Items([
+                            new NativeMenuItem()
+                                .Header(Resources.MainView_Menu_Window_ResetLayout)
+                                .Command(vm.ResetLayoutCommand),
+                            new NativeMenuItem()
+                                .Header(Resources.MainView_Menu_Window_SwitchTheme)
+                                .Command(vm.SwitchThemeCommand),
+                            ]),
+                    new NativeMenuItem()
+                        .Header(Resources.MainView_Menu_Help)
+                        .Items([
+                            new NativeMenuItem()
+                                .Header(Resources.MainView_Menu_Help_About)
+                                .Command(vm.OpenAboutCommand),
+                            ]),
+                    ])
             )
             .Content(
-                new Panel()
-                    .Children([
-                        new MainView()
-                    ]
-                )
+                new MainView()
             );
 }
-
 
 public static class WindowExtensions
 {
