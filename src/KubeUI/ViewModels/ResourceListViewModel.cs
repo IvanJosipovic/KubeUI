3ebec76c81eba6618673653c0b017d7cd29254a6
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-09-24 09:12:20 -0700

    fix(V1ClusterRoleBinding): Bindings Column

--- src/KubeUI/ViewModels/ResourceListViewModel.cs
+++ src/KubeUI/ViewModels/ResourceListViewModel.cs
@@ -1127,7 +1127,7 @@ public partial class ResourceListViewModel<T> : ViewModelBase, IInitializeCluste
                 new ResourceListViewDefinitionColumn<V1ClusterRoleBinding, string>()
                 {
                     Name = "Bindings",
-                    Field = x => x.Subjects.Select(y => y.Name).Aggregate((a,b) => a + ", " + b),
+                    Field = x => x.Subjects == null || x.Subjects.Count == 0 ? "" : x.Subjects.Select(y => y.Name).Aggregate((a,b) => a + ", " + b),
                     Width = "*",
                 },
                 AgeColumn()
