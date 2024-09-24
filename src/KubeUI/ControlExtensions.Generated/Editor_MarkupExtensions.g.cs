e60736c1318e2e839f6ed4ad2fc1cd5649a5bbb3
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-09-23 17:56:43 -0700

    chore: update extensions (#726)

---
+++ src/KubeUI/ControlExtensions.Generated/Editor_MarkupExtensions.g.cs
@@ -0,0 +1,122 @@
+#nullable enable
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Linq.Expressions;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Avalonia.Markup.Declarative;
+[global::System.CodeDom.Compiler.GeneratedCode("AvaloniaExtensionGenerator", "11.1.3.0")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public static partial class Editor_MarkupExtensions
+{
+//================= Properties ======================//
+ // ZoomControlProperty
+
+/*BindFromExpressionSetterGenerator*/
+public static T ZoomControl<T>(this T control, Func<NodeEditor.Controls.NodeZoomBorder> func, Action<NodeEditor.Controls.NodeZoomBorder>? onChanged = null, [CallerArgumentExpression("func")] string? expression = null) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.ZoomControlProperty, func, onChanged, expression);
+
+/*MagicalSetterGenerator*/
+public static T ZoomControl<T>(this T control, NodeEditor.Controls.NodeZoomBorder value, BindingMode? bindingMode = null, IValueConverter? converter = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.Editor
+=> control._setEx(NodeEditor.Controls.Editor.ZoomControlProperty, ps, () => control.ZoomControl = value, bindingMode, converter, bindingSource);
+
+/*BindSetterGenerator*/
+public static T ZoomControl<T>(this T control, IBinding binding) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.ZoomControlProperty, binding);
+
+/*AvaloniaPropertyBindSetterGenerator*/
+public static T ZoomControl<T>(this T control, AvaloniaProperty avaloniaProperty, BindingMode? bindingMode = null, IValueConverter? converter = null, ViewBase? overrideView = null) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.ZoomControlProperty, avaloniaProperty, bindingMode, converter, overrideView);
+
+/*MagicalSetterWithConverterGenerator*/
+public static T ZoomControl<T,TValue>(this T control, TValue value, FuncValueConverter<TValue, NodeEditor.Controls.NodeZoomBorder> converter, BindingMode? bindingMode = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.Editor
+=> control._setEx(NodeEditor.Controls.Editor.ZoomControlProperty, ps, () => control.ZoomControl = converter.TryConvert(value), bindingMode, converter, bindingSource);
+
+
+ // DrawingNodeProperty
+
+/*BindFromExpressionSetterGenerator*/
+public static T DrawingNode<T>(this T control, Func<NodeEditor.Controls.DrawingNode> func, Action<NodeEditor.Controls.DrawingNode>? onChanged = null, [CallerArgumentExpression("func")] string? expression = null) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.DrawingNodeProperty, func, onChanged, expression);
+
+/*MagicalSetterGenerator*/
+public static T DrawingNode<T>(this T control, NodeEditor.Controls.DrawingNode value, BindingMode? bindingMode = null, IValueConverter? converter = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.Editor
+=> control._setEx(NodeEditor.Controls.Editor.DrawingNodeProperty, ps, () => control.DrawingNode = value, bindingMode, converter, bindingSource);
+
+/*BindSetterGenerator*/
+public static T DrawingNode<T>(this T control, IBinding binding) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.DrawingNodeProperty, binding);
+
+/*AvaloniaPropertyBindSetterGenerator*/
+public static T DrawingNode<T>(this T control, AvaloniaProperty avaloniaProperty, BindingMode? bindingMode = null, IValueConverter? converter = null, ViewBase? overrideView = null) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.DrawingNodeProperty, avaloniaProperty, bindingMode, converter, overrideView);
+
+/*MagicalSetterWithConverterGenerator*/
+public static T DrawingNode<T,TValue>(this T control, TValue value, FuncValueConverter<TValue, NodeEditor.Controls.DrawingNode> converter, BindingMode? bindingMode = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.Editor
+=> control._setEx(NodeEditor.Controls.Editor.DrawingNodeProperty, ps, () => control.DrawingNode = converter.TryConvert(value), bindingMode, converter, bindingSource);
+
+
+ // AdornerCanvasProperty
+
+/*BindFromExpressionSetterGenerator*/
+public static T AdornerCanvas<T>(this T control, Func<Avalonia.Controls.Canvas> func, Action<Avalonia.Controls.Canvas>? onChanged = null, [CallerArgumentExpression("func")] string? expression = null) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.AdornerCanvasProperty, func, onChanged, expression);
+
+/*MagicalSetterGenerator*/
+public static T AdornerCanvas<T>(this T control, Avalonia.Controls.Canvas value, BindingMode? bindingMode = null, IValueConverter? converter = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.Editor
+=> control._setEx(NodeEditor.Controls.Editor.AdornerCanvasProperty, ps, () => control.AdornerCanvas = value, bindingMode, converter, bindingSource);
+
+/*BindSetterGenerator*/
+public static T AdornerCanvas<T>(this T control, IBinding binding) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.AdornerCanvasProperty, binding);
+
+/*AvaloniaPropertyBindSetterGenerator*/
+public static T AdornerCanvas<T>(this T control, AvaloniaProperty avaloniaProperty, BindingMode? bindingMode = null, IValueConverter? converter = null, ViewBase? overrideView = null) where T : NodeEditor.Controls.Editor
+   => control._set(NodeEditor.Controls.Editor.AdornerCanvasProperty, avaloniaProperty, bindingMode, converter, overrideView);
+
+/*MagicalSetterWithConverterGenerator*/
+public static T AdornerCanvas<T,TValue>(this T control, TValue value, FuncValueConverter<TValue, Avalonia.Controls.Canvas> converter, BindingMode? bindingMode = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.Editor
+=> control._setEx(NodeEditor.Controls.Editor.AdornerCanvasProperty, ps, () => control.AdornerCanvas = converter.TryConvert(value), bindingMode, converter, bindingSource);
+
+
+
+//================= Events ======================//
+
+//================= Styles ======================//
+ // ZoomControlProperty
+
+/*ValueStyleSetterGenerator*/
+public static Style<T> ZoomControl<T>(this Style<T> style, NodeEditor.Controls.NodeZoomBorder value) where T : NodeEditor.Controls.Editor
+=> style._addSetter(NodeEditor.Controls.Editor.ZoomControlProperty, value);
+
+/*BindingStyleSetterGenerator*/
+public static Style<T> ZoomControl<T>(this Style<T> style, IBinding binding) where T : NodeEditor.Controls.Editor
+=> style._addSetter(NodeEditor.Controls.Editor.ZoomControlProperty, binding);
+
+
+ // DrawingNodeProperty
+
+/*ValueStyleSetterGenerator*/
+public static Style<T> DrawingNode<T>(this Style<T> style, NodeEditor.Controls.DrawingNode value) where T : NodeEditor.Controls.Editor
+=> style._addSetter(NodeEditor.Controls.Editor.DrawingNodeProperty, value);
+
+/*BindingStyleSetterGenerator*/
+public static Style<T> DrawingNode<T>(this Style<T> style, IBinding binding) where T : NodeEditor.Controls.Editor
+=> style._addSetter(NodeEditor.Controls.Editor.DrawingNodeProperty, binding);
+
+
+ // AdornerCanvasProperty
+
+/*ValueStyleSetterGenerator*/
+public static Style<T> AdornerCanvas<T>(this Style<T> style, Avalonia.Controls.Canvas value) where T : NodeEditor.Controls.Editor
+=> style._addSetter(NodeEditor.Controls.Editor.AdornerCanvasProperty, value);
+
+/*BindingStyleSetterGenerator*/
+public static Style<T> AdornerCanvas<T>(this Style<T> style, IBinding binding) where T : NodeEditor.Controls.Editor
+=> style._addSetter(NodeEditor.Controls.Editor.AdornerCanvasProperty, binding);
+
+
+
+}
---
+++ src/KubeUI/ControlExtensions.Generated/SelectionAdorner_MarkupExtensions.g.cs
@@ -0,0 +1,88 @@
+#nullable enable
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using System;
+using System.Linq.Expressions;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Avalonia.Markup.Declarative;
+[global::System.CodeDom.Compiler.GeneratedCode("AvaloniaExtensionGenerator", "11.1.3.0")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public static partial class SelectionAdorner_MarkupExtensions
+{
+//================= Properties ======================//
+ // TopLeftProperty
+
+/*BindFromExpressionSetterGenerator*/
+public static T TopLeft<T>(this T control, Func<Avalonia.Point> func, Action<Avalonia.Point>? onChanged = null, [CallerArgumentExpression("func")] string? expression = null) where T : NodeEditor.Controls.SelectionAdorner
+   => control._set(NodeEditor.Controls.SelectionAdorner.TopLeftProperty, func, onChanged, expression);
+
+/*MagicalSetterGenerator*/
+public static T TopLeft<T>(this T control, Avalonia.Point value, BindingMode? bindingMode = null, IValueConverter? converter = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.SelectionAdorner
+=> control._setEx(NodeEditor.Controls.SelectionAdorner.TopLeftProperty, ps, () => control.TopLeft = value, bindingMode, converter, bindingSource);
+
+/*BindSetterGenerator*/
+public static T TopLeft<T>(this T control, IBinding binding) where T : NodeEditor.Controls.SelectionAdorner
+   => control._set(NodeEditor.Controls.SelectionAdorner.TopLeftProperty, binding);
+
+/*AvaloniaPropertyBindSetterGenerator*/
+public static T TopLeft<T>(this T control, AvaloniaProperty avaloniaProperty, BindingMode? bindingMode = null, IValueConverter? converter = null, ViewBase? overrideView = null) where T : NodeEditor.Controls.SelectionAdorner
+   => control._set(NodeEditor.Controls.SelectionAdorner.TopLeftProperty, avaloniaProperty, bindingMode, converter, overrideView);
+
+/*MagicalSetterWithConverterGenerator*/
+public static T TopLeft<T,TValue>(this T control, TValue value, FuncValueConverter<TValue, Avalonia.Point> converter, BindingMode? bindingMode = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.SelectionAdorner
+=> control._setEx(NodeEditor.Controls.SelectionAdorner.TopLeftProperty, ps, () => control.TopLeft = converter.TryConvert(value), bindingMode, converter, bindingSource);
+
+
+ // BottomRightProperty
+
+/*BindFromExpressionSetterGenerator*/
+public static T BottomRight<T>(this T control, Func<Avalonia.Point> func, Action<Avalonia.Point>? onChanged = null, [CallerArgumentExpression("func")] string? expression = null) where T : NodeEditor.Controls.SelectionAdorner
+   => control._set(NodeEditor.Controls.SelectionAdorner.BottomRightProperty, func, onChanged, expression);
+
+/*MagicalSetterGenerator*/
+public static T BottomRight<T>(this T control, Avalonia.Point value, BindingMode? bindingMode = null, IValueConverter? converter = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.SelectionAdorner
+=> control._setEx(NodeEditor.Controls.SelectionAdorner.BottomRightProperty, ps, () => control.BottomRight = value, bindingMode, converter, bindingSource);
+
+/*BindSetterGenerator*/
+public static T BottomRight<T>(this T control, IBinding binding) where T : NodeEditor.Controls.SelectionAdorner
+   => control._set(NodeEditor.Controls.SelectionAdorner.BottomRightProperty, binding);
+
+/*AvaloniaPropertyBindSetterGenerator*/
+public static T BottomRight<T>(this T control, AvaloniaProperty avaloniaProperty, BindingMode? bindingMode = null, IValueConverter? converter = null, ViewBase? overrideView = null) where T : NodeEditor.Controls.SelectionAdorner
+   => control._set(NodeEditor.Controls.SelectionAdorner.BottomRightProperty, avaloniaProperty, bindingMode, converter, overrideView);
+
+/*MagicalSetterWithConverterGenerator*/
+public static T BottomRight<T,TValue>(this T control, TValue value, FuncValueConverter<TValue, Avalonia.Point> converter, BindingMode? bindingMode = null, object? bindingSource = null, [CallerArgumentExpression("value")] string? ps = null) where T : NodeEditor.Controls.SelectionAdorner
+=> control._setEx(NodeEditor.Controls.SelectionAdorner.BottomRightProperty, ps, () => control.BottomRight = converter.TryConvert(value), bindingMode, converter, bindingSource);
+
+
+
+//================= Events ======================//
+
+//================= Styles ======================//
+ // TopLeftProperty
+
+/*ValueStyleSetterGenerator*/
+public static Style<T> TopLeft<T>(this Style<T> style, Avalonia.Point value) where T : NodeEditor.Controls.SelectionAdorner
+=> style._addSetter(NodeEditor.Controls.SelectionAdorner.TopLeftProperty, value);
+
+/*BindingStyleSetterGenerator*/
+public static Style<T> TopLeft<T>(this Style<T> style, IBinding binding) where T : NodeEditor.Controls.SelectionAdorner
+=> style._addSetter(NodeEditor.Controls.SelectionAdorner.TopLeftProperty, binding);
+
+
+ // BottomRightProperty
+
+/*ValueStyleSetterGenerator*/
+public static Style<T> BottomRight<T>(this Style<T> style, Avalonia.Point value) where T : NodeEditor.Controls.SelectionAdorner
+=> style._addSetter(NodeEditor.Controls.SelectionAdorner.BottomRightProperty, value);
+
+/*BindingStyleSetterGenerator*/
+public static Style<T> BottomRight<T>(this Style<T> style, IBinding binding) where T : NodeEditor.Controls.SelectionAdorner
+=> style._addSetter(NodeEditor.Controls.SelectionAdorner.BottomRightProperty, binding);
+
+
+
+}
