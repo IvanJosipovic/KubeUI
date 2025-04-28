7ab8582c496241a19579b9e60ae269d0918e8ec6
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-04-27 20:23:49 -0700

    chore: pin kind node version

--- tests/KubeUI.Tests/TestHarness.cs
+++ tests/KubeUI.Tests/TestHarness.cs
@@ -24,7 +24,7 @@ public class TestHarness : IDisposable
     {
         Kind = new Kind();
         await Kind.DownloadClient();
-        await Kind.CreateCluster(Name);
+        await Kind.CreateCluster(Name, "kindest/node:v1.32.3");
 
         KubeConfig = await Kind.GetK8SConfiguration(Name);
 
