d090fdc4323b84d924b9222f7cc7a6e3f2f22f2d
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-10-21 18:05:45 -0700

    wip

---
+++ src/KubeUI/Views/VisualizationResourceView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace KubeUI.Views;
+
+public sealed partial class VisualizationResourceView : UserControl
+{
+    public VisualizationResourceView()
+    {
+        InitializeComponent();
+    }
+}
