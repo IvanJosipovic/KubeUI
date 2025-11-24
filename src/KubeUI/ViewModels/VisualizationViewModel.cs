8908a98da26639c7f877994f9f53f08e9dd0a9ce
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-11-24 08:34:53 -0800

    wip

--- src/KubeUI/ViewModels/VisualizationViewModel.cs
+++ src/KubeUI/ViewModels/VisualizationViewModel.cs
@@ -137,8 +137,10 @@ public sealed partial class VisualizationViewModel : ViewModelBase, IInitializeC
 
                 // HideNoise
                 if (HideNoise
-                    && value is V1ReplicaSet replicaSet && replicaSet.Status.Replicas == 0
-                    && value is Corev1Event)
+                    && (
+                           (value is V1ReplicaSet replicaSet && replicaSet.Status.Replicas == 0)
+                        || (value is Corev1Event)
+                    ))
                 {
                     continue;
                 }
