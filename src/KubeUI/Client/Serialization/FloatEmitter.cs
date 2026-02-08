0587b71fce30795a2aad84671afbb51e98a1ce07
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-02-07 21:44:07 -0800

    feat(yaml): improve parsing, serialization, and type support
    - Use CultureInfo.InvariantCulture for float formatting
    - Add KubernetesDateTime/DateTimeOffset type converters
    - Switch to MergingParser for YAML anchor/merge support
    - Enhance resource management with await using/using
    - Determine object types from apiVersion/kind in YAML
    - Adjust serializer event emitter order and add converters

--- src/KubeUI/Client/Serialization/FloatEmitter.cs
+++ src/KubeUI/Client/Serialization/FloatEmitter.cs
@@ -1,4 +1,5 @@
-﻿using YamlDotNet.Core;
+﻿using System.Globalization;
+using YamlDotNet.Core;
 using YamlDotNet.Core.Events;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.EventEmitters;
@@ -18,10 +19,10 @@ internal class FloatEmitter : ChainedEventEmitter
         {
             // Floating point numbers should always render at least one zero (e.g. 1.0f => '1.0' not '1')
             case double d:
-                emitter.Emit(new Scalar(d.ToString("0.0######################")));
+                emitter.Emit(new Scalar(d.ToString("0.0######################", CultureInfo.InvariantCulture)));
                 break;
             case float f:
-                emitter.Emit(new Scalar(f.ToString("0.0######################")));
+                emitter.Emit(new Scalar(f.ToString("0.0######################", CultureInfo.InvariantCulture)));
                 break;
             default:
                 base.Emit(eventInfo, emitter);
