dbae019c96712db905a937e33121e6a17a5bb41a
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-02-22 00:26:34 -0800

    wip

--- benchmarks/KubeUI.Benchmarks/ClusterAuthBenchmarks.cs
+++ benchmarks/KubeUI.Benchmarks/ClusterAuthBenchmarks.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
 using k8s.Models;
@@ -15,11 +14,11 @@ public class ClusterAuthBenchmarks
     [GlobalSetup]
     public void Setup()
     {
-        _index = new Dictionary<string, V1SelfSubjectAccessReview>();
+        _index = [];
 
         for (int i = 0; i < 1000; i++)
         {
-            var review = new V1SelfSubjectAccessReview()
+            var review = new V1SelfSubjectAccessReview
             {
                 Spec = new V1SelfSubjectAccessReviewSpec()
                 {
@@ -32,18 +31,12 @@ public class ClusterAuthBenchmarks
                         Verb = "list",
                         Version = "v1"
                     }
+                },
+                Status = new V1SubjectAccessReviewStatus
+                {
+                    Allowed = (i % 3 == 0)
                 }
             };
-
-            // set status via reflection
-            var statusType = typeof(V1SelfSubjectAccessReview).Assembly.GetType("k8s.Models.V1SelfSubjectAccessReviewStatus");
-            if (statusType != null)
-            {
-                var status = Activator.CreateInstance(statusType)!;
-                var allowedProp = statusType.GetProperty("Allowed");
-                allowedProp?.SetValue(status, (i % 3 == 0));
-                typeof(V1SelfSubjectAccessReview).GetProperty("Status")?.SetValue(review, status);
-            }
 
             var key = Cluster.BuildReviewKeyFromParts("", "pods", "v1", "list", (i % 10 == 0 ? null : $"ns{i % 10}"), null);
             _index[key] = review;
